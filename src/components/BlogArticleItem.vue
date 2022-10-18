394697d9f7c457397c86bb853a6418c966b5f537
Filip-Papic <filippapic97@gmail.com>
2022-10-18 12:29:19 +0200

    blog done

--- src/components/BlogArticleItem.vue
+++ src/components/BlogArticleItem.vue
@@ -24,10 +24,74 @@ const imgSrc = computed(() => {
       <h4>
         <b>{{ title }}</b>
       </h4>
-      <p>some description...</p>
-      <button class="button">Add to cart</button>
+      <p>
+        Long Jing tea (龙井茶), also known by its literal translation, “Dragon
+        Well”, is a variety of pan-fried green tea from Hangzhou, China’s
+        Zhejiang Province. It can be argued that Long Jing is the most
+        prestigious Chinese green tea; in the Qing Dynasty, Long Jing was given
+        the status of “Imperial Tea”.
+      </p>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  overflow: hidden;
+  margin-bottom: 1.5rem;
+  margin-top: 1rem;
+  margin-right: 11.7%;
+  margin-left: 11.7%;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+.container {
+  padding: 0px 16px 16px 16px;
+}
+h4 {
+  font-family: "Montserrat", sans-serif;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+}
+p {
+  font-family: "Montserrat";
+  font-size: 1.2rem;
+  font-weight: 400;
+  margin-bottom: 0.5rem;
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+img:hover {
+  filter: brightness(50%);
+}
+.overflow {
+  width: 100%;
+  height: 100%;
+  max-width: 30%;
+  min-width: 30%;
+  overflow: hidden;
+}
+@media screen and (max-width: 800px) {
+  .card {
+    margin-right: 5%;
+    margin-left: 5%;
+    flex-direction: column;
+  }
+  img {
+    width: 100%;
+  }
+  .overflow {
+    min-width: 100%;
+  }
+}
+</style>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -5,10 +5,48 @@ import BlogArticleItem from "../components/BlogArticleItem.vue";
 <template>
   <div>
     <BlogArticleItem
-      title="Milk Oolong: Tea Of The Month"
-      img="oolong-tea-cat.jpg"
+      title="Dragon Well: Tea Of The Month"
+      img="green-tea-cat.jpg"
     />
+    <BlogArticleItem
+      title="Butterfly Pea: Tea Of The Month"
+      img="fruit-tea-cat.jpg"
+    />
+    <BlogArticleItem
+      title="Learn About Tea: Blending"
+      img="black-tea-cat.jpg"
+    />
+    <div class="pagination">
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+}
+.pagination a {
+  color: black;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+.pagination a.active {
+  background-color: #4caf50;
+  color: white;
+}
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
+}
+</style>
